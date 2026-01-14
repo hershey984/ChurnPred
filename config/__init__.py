c0757817f040ecc0266f1943e61b4aaa2756fca8
hershey984 <harshsawant984@gmail.com>
2026-01-14 23:32:54 +0530

    ...

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+# src/config/__init__.py
+from .schemas import (
+    DROP_COLS,
+    TARGET_COL,
+    NUM_COLS,
+    CAT_COLS,
+    BIN_COLS
+)
+
+
+
+
---
+++ config/schemas.py
@@ -0,0 +1,21 @@
+#columns to drop
+DROP_COLS = ["CustomerID"]
+
+#target column(s)
+TARGET_COL = "Churn"
+
+#numeric columns
+NUM_COLS = [
+
+]
+
+#categorical columns
+CAT_COLS = [
+
+]
+
+#binary columns
+BIN_COLS = [
+
+]
+
