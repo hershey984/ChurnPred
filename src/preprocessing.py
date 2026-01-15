b924d7c52c9dbf63d6e18418e794ba0936abb933
xzysst <owaisrizvi42@gmail.com>
2026-01-15 16:26:19 +0530

    Added preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+from config.schemas import TARGET_COL
+
+def preprocess_data(file_path: str):
+    try:
+        df = pd.read_excel(file_path)
+        print("----------HEAD----------")
+        print(df.head())
+        dup = df.duplicated().sum()
+        if dup > 0:
+            print(f"Dropping {dup} duplicate rows")
+            df = df.drop_duplicates()
+        
+        print("Separating Features and Target")
+        X = df.drop(columns=[TARGET_COL])
+        y = df[TARGET_COL]
+        
+        numerical_cols = X.select_dtypes(include=['int64','float64']).columns
+        cat_cols = X.select_dtypes(include=['object','bool','category']).columns
+        
+        num_imputer = SimpleImputer(strategy="median")
+        cat_imputer = SimpleImputer(strategy="most_frequent")
+        X[numerical_cols] = num_imputer.fit_transform(X[numerical_cols])
+        X[cat_cols] = cat_imputer.fit_transform(X[cat_cols])
+        
+        return X,y
+    
+    except Exception as e:
+        print(f"Failed to preprocess data {e}")
+        raise
+        
+
+
